11c22f2c42e6e7c0b6023d2380b506957cfe8b67
Fernando Salom <fer@rudo.es>
2023-04-14 19:15:15 +0200

    Add character data model

--- withoutDI/app/src/main/java/com/example/clean/data/model/Character.kt
+++ withoutDI/app/src/main/java/com/example/clean/data/model/Character.kt
@@ -1,2 +1,10 @@
 package com.example.clean.data.model
 
+data class Character(
+    val id: Int = 0,
+    val name: String = "",
+    val status: String = "",
+    val species: String = "",
+    val image: String = "",
+    val episodes: List<String> = listOf()
+)
